04e5c5ee641caf1620548426d634c6b84f5d3a1c
Janak Shah <janak.shah@cuvva.com>
2021-03-13 19:47:49 +0000

    fix deprecated Data(bytes: in TestModel

--- RetailAppTests/Mocks/TestModel.swift
+++ RetailAppTests/Mocks/TestModel.swift
@@ -2,6 +2,6 @@ import Foundation
 
 struct TestModel: Decodable, Equatable {
   let value: Int
-  static var data = Data(bytes: "{\"value\": 1}".utf8)
+  static var data = Data("{\"value\": 1}".utf8)
   static var model = TestModel(value: 1)
 }
