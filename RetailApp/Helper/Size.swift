40749eff7ff58858cb570b67d95fd1ab3ad0b764
Janak Shah <janak.shah@cuvva.com>
2021-03-14 10:06:29 +0000

    adding Size abstraction and Theme placeholder file

---
+++ RetailApp/Helper/Size.swift
@@ -0,0 +1,32 @@
+//
+//  Size.swift
+//  RetailApp
+//
+//  Created by Janak Shah on 14/03/2021.
+//  Copyright © 2021 Marks and Spencer. All rights reserved.
+//
+
+import UIKit
+
+struct Size {
+    
+    // MARK: - Spacings
+    
+    ///Returns CGFloat 4
+    static let spacingExtraSmall: CGFloat = 4
+    
+    ///Returns CGFloat 8
+    static let spacingSmall: CGFloat = 8
+        
+    ///Returns CGFloat 12
+    static let spacingRegular: CGFloat = 12
+    
+    ///Returns CGFloat 16
+    static let spacingLarge: CGFloat = 16
+
+    // MARK: - Paddings
+    
+    ///Returns CGFloat 20
+    static let standardEdgePadding: CGFloat = 20
+    
+}
---
+++ RetailApp/Helper/Theme.swift
@@ -0,0 +1,9 @@
+//
+//  Theme.swift
+//  RetailApp
+//
+//  Created by Janak Shah on 14/03/2021.
+//  Copyright © 2021 Marks and Spencer. All rights reserved.
+//
+
+import Foundation
